f66955ead220d2db15a452a50cd98318a79fee99
rakesha.s <rakesha.s@zohocorp.com>
2025-06-09 15:30:27 +0530

    Updated newPrint and deprecated old print

--- Sources/Core/Core.swift
+++ Sources/Core/Core.swift
@@ -2,7 +2,13 @@ public class Core {
 
     public init() {}
 
+    @available(*, deprecated, renamed: "secondPrint", message: "Use the newer second print function instead")
     public func print() -> String {
         return "2"
     }
+
+    public func newPrint() -> String {
+        return "2"
+    }
+
 }
